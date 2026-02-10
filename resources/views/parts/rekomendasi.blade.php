4a8e7d6ad66b9f214af9ff39191c032dda1a3409
Andre <andremm73@gmail.com>
2026-02-10 10:01:36 +0700

    Perbaikan UI Halaman Depan

--- resources/views/parts/rekomendasi.blade.php
+++ resources/views/parts/rekomendasi.blade.php
@@ -6,54 +6,10 @@
         {{-- Rekomendasi Kekasih --}}
         <div class="row mt-5">
             <div class="col-lg-6 col-sm-5">
-                <p class="fw-semibold fs-4 mb-0">Rekomendasi untuk Kekasih</p>
+                <p class="fw-semibold fs-4 mb-0">List Hadiah</p>
             </div>
             <div class="col-lg-6 col-sm-7 text-end align-self-center">
-                <a href="" class="text-decoration-none text-muted text-end">Lihat Semua...</a>
-            </div>
-        </div>
-        <div class="row mt-3">
-            @foreach ($produk as $data)
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <div class="card product-card">
-                    <!-- Image -->
-                    <div class="position-relative">
-                        <img src="{{ asset('assets/img/Produk/'. $data->foto_1) }}" class="card-img-top" alt="Keranjang Bunga Mawar">
-
-                        <!-- Favorit badge -->
-                        <span class="badge-favorit">
-                            <i class="fa-solid fa-award"></i>&nbsp;&nbsp;Verified
-                        </span>
-                    </div>
-
-                    <!-- Body -->
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between align-items-center mb-1">
-                            <span class="category-text">{{ $data->kategori->nama_kategori }}</span>
-                            <span class="rating-text">
-                                <i class="fa-solid fa-star text-warning"></i> 4.7
-                            </span>
-                        </div>
-
-                        <p class="product-title">{{ $data->nama_produk }}</p>
-
-                        <p class="product-price">Rp {{ number_format($data->harga, 0, ',', '.') }}</p>
-
-                        <a href="{{ route('detail', $data->id) }}" class="btn btn-detail w-100">Lihat Detail</a>
-                    </div>
-                </div>
-            </div>
-            @endforeach
-
-        </div>
-
-        {{-- Rekomendasi Orang Tua --}}
-        <div class="row mt-5">
-            <div class="col-lg-6 col-sm-5">
-                <p class="fw-semibold fs-4 mb-0">Rekomendasi untuk Orang Tua</p>
-            </div>
-            <div class="col-lg-6 col-sm-7 text-end align-self-center">
-                <a href="" class="text-decoration-none text-muted text-end">Lihat Semua...</a>
+                <a href="{{ route('etalase') }}" class="text-decoration-none text-muted text-end">Lihat Semua...</a>
             </div>
         </div>
         <div class="row mt-3">
